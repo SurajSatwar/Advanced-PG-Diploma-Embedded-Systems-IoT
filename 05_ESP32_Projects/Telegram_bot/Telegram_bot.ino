ddcb79ea46a2959204a2c9d3d28c61a6d8f2cfa9
Suraj Satwar <surajsatwar@gmail.com>
2025-09-25 23:33:50 +0530

    Initial commit

---
+++ 05_ESP32_Projects/Telegram_bot/Telegram_bot.ino
@@ -0,0 +1,91 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+// Wifi network station credentials
+#define WIFI_SSID "184527"
+#define WIFI_PASSWORD "184527184527"
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "8319340811:AAH-Z2NE5FZZwRkkW-oGuqinVfGf3Qnp7A0"
+
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+unsigned long bot_lasttime;          // last time messages' scan has been done
+bool Start = false;
+
+void handleNewMessages(int numNewMessages)
+{
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String chat_id = bot.messages[i].chat_id;
+    String text = bot.messages[i].text;
+
+    String from_name = bot.messages[i].from_name;
+    if (from_name == "")
+      from_name = "Guest";
+
+    if (text == "Led on")
+    {
+      digitalWrite(2,HIGH);
+      bot.sendMessage(chat_id, "Turned on Led");
+    }
+    if (text == "Led off")
+    {
+      digitalWrite(2,LOW);
+      bot.sendMessage(chat_id, "Turned off Led");
+    }
+
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+pinMode(2,OUTPUT);
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+}
+
+void loop()
+{
+  if (millis() - bot_lasttime > BOT_MTBS)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages)
+    {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    bot_lasttime = millis();
+  }
+}
